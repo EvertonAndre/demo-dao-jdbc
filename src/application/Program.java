183fceb09127ccd83b2652d27bacdcf61b38c1e2
Everton Andre <evertonandre1996@gmail.com>
2022-07-08 10:52:00 -0300

    Seller and entityClass

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
 
@@ -9,5 +13,7 @@ public class Program {
         Department obj = new Department(1,"Books");
         System.out.println(obj);
 
+        Seller obj2= new Seller(2,"Joao","gfjgf@gmail",new Date(),600.00, obj);
+        System.out.println(obj2);
     }
 }
