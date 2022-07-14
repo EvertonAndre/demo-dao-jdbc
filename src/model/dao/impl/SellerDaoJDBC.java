c9065934fbaeeefd55286ff58a4a1ecf94b99024
Everton Andre <evertonandre1996@gmail.com>
2022-07-14 10:46:03 -0300

    findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -90,8 +90,45 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "ORDER BY Name");
+
+            rs = st.executeQuery();
+            //Lista para armazenar os resultados
+            List<Seller> list = new ArrayList<>();
+            // Map vazio para guardar qualquer departamento que eu instanciar
+            Map<Integer, Department> map = new HashMap<>();
+            //Enquanto tiver resultados percorra meu resultSet
+
+            while (rs.next()) {
+                //Toda vez que passar pelo meu While, eu tento buscar o ID do departamento no Map para saber se ja existe ou nao
+                //Se o Map retornar nulo, ai sim vou instanciar o departamento. Caso contrario o departamento ja existe
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if(dep == null){
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
+
 
     @Override
     public List<Seller> findByDepartament(Department departament) {
