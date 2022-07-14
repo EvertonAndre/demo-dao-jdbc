f0d90857534e1c56dab537c6e568261d781997c6
Everton Andre <evertonandre1996@gmail.com>
2022-07-14 10:00:55 -0300

    findByDepartment Implementatio

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
     // crio um objeto Connection como uma atributo
@@ -88,5 +91,48 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return null;
+    }
+
+    @Override
+    public List<Seller> findByDepartament(Department departament) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name");
+            st.setInt(1, departament.getId());
+
+            rs = st.executeQuery();
+            //Lista para armazenar os resultados
+            List<Seller> list = new ArrayList<>();
+            // Map vazio para guardar qualquer departamento que eu instanciar
+            Map<Integer, Department> map = new HashMap<>();
+            //Enquanto tiver resultados percorra meu resultSet
+
+            while (rs.next()) {
+                //Toda vez que passar pelo meu While, eu tento buscar o ID do departamento no Map para saber se ja existe ou nao
+                //Se o Map retornar nulo, ai sim vou instanciar o departamento. Caso contrario o departamento ja existe
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if(dep == null){
+                   dep = instantiateDepartment(rs);
+                   map.put(rs.getInt("DepartmentId"), dep);
+               }
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 }
